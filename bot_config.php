1a165d3073aae2bc94e8bed0ca10c98c2a143e93
Dinar Dirmanto <33767621+dirmanto@users.noreply.github.com>
2017-11-19 22:20:57 +0700

    Create bot_config.php

---
+++ bot_config.php
@@ -0,0 +1,23 @@
+<?php 
+/* 
+**********************************
+**** Variabili API Telegram ******
+**********************************
+*/
+/* Token API Telegram. Silahkan Ambil Dari @BotFather */ 
+$token = '344428677:AAGiu_nIJLENQ1LdHKt0TVdL_ccyjiYDNJo';
+
+/* Isi Dengan Grup ID */
+$chat = '-1001228509159'; 
+/* Sumber RSS Feed */
+$rss = 'https://feeds.feedburner.com/kangdirmanto';
+
+/* Log Disimpan */
+$log_file = 'channel_bot.log';
+
+/* Proses PID Bot */
+$pid_file = 'bot.pid';
+
+/* Timer Waktu */
+$attesa = 60;
+?>
